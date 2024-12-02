6f504576c8a4239f72ce79b95b13087011e7bac4
lagrandecode <seunogunmolufirst1@gmail.com>
2024-12-01 20:53:24 -0500

    "commit"

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
       {/* Content Section */}
       <div className="flex-1 flex" style={{ fontFamily: "'Poppins', sans-serif" }}>
         {/* Text Section */}
-        <div className="w-1/2 flex flex-col justify-center items-center text-center px-6 bg-white">
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-center text-center px-6 bg-white">
           <h1 className="text-4xl md:text-5xl font-bold mb-4 text-gray-900">
             The practice test program that will help you ace your Ontario G1
           </h1>
@@ -22,10 +22,10 @@ const Home = () => {
             This is a group project developed by Humber College Students 2024.
           </p>
 
-          <div className="space-y-4">
+          <div className="space-y-4 w-full">
             <Link
               to="/quiz"
-              className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white font-semibold py-3 px-6 rounded-lg hover:from-red-500 hover:to-red-600 shadow-md"
+              className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white font-semibold py-3 px-6 rounded-lg hover:from-red-500 hover:to-red-600 shadow-md w-full sm:w-auto text-center"
             >
               Start free G1 practice test
             </Link>
@@ -34,7 +34,7 @@ const Home = () => {
 
         {/* Background Image Section */}
         <div
-          className="w-1/2"
+          className="w-full md:w-1/2"
           style={{
             backgroundImage: `url(${TrafficBackground})`,
             backgroundSize: "cover",
